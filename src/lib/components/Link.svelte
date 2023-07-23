d94bee273c9355a174591a01ab30e287be4f9b43
AB <brainteazer123@gmail.com>
2023-07-23 14:46:00 +0200

    cleaned up code

--- src/lib/components/Link.svelte
+++ src/lib/components/Link.svelte
@@ -5,12 +5,6 @@
   export let title: string = "";
 </script>
 
-{#if onclick == null}
-  <a {href} target="_blank" rel="noopener noreferrer" class={classProps} {title}>
-    <slot />
-  </a>
-{:else}
-  <a {href} target="_blank" rel="noopener noreferrer" class={classProps} on:click|preventDefault={onclick}>
-    <slot />
-  </a>
-{/if}
+<a {href} target="_blank" rel="noopener noreferrer" class={classProps} {title} on:click={onclick}>
+  <slot />
+</a>
