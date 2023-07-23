8186cfbc68c18546ff5ff1788e47d44897337ba1
AB <brainteazer123@gmail.com>
2023-07-23 14:46:00 +0200

    cleaned up code

--- src/lib/functions/scrollIntoView.ts
+++ src/lib/functions/scrollIntoView.ts
@@ -1,4 +1,5 @@
 export const scrollIntoView = ({ currentTarget }: any) => {
+  event?.preventDefault();
   const scrollToElement = document.querySelector(currentTarget.getAttribute("href"));
 
   if (!scrollToElement) return;
