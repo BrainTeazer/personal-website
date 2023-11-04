73df58ece83991794b8f62d577123759b28da161
AB <brainteazer123@gmail.com>
2023-11-04 15:09:01 +0100

    initial design of blog

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import Card from "$lib/components/Card.svelte";
   import Navbar from "$lib/components/Navbar.svelte";
   import navbarItemProp from "$lib/data/navbarItemProp";
   export let data: any;
@@ -6,9 +7,11 @@
 
 <div class="flex flex-col items-center">
   <Navbar navbarItems={navbarItemProp} />
-  {#each data.posts as post}
-    <a href="/blog/{post.slug}">
-      {post.title}
-    </a>
-  {/each}
+  <div class="flex flex-col items-center w-3/4">
+    {#each data.posts as post}
+      <a href="/blog/{post.slug}" class="self-start">
+        <Card title={post.title} classProp={"m-2"} />
+      </a>
+    {/each}
+  </div>
 </div>
