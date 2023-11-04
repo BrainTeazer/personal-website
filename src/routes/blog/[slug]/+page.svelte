c91a74dd01317f0fc45b8235f6e60f81188c0e68
AB <brainteazer123@gmail.com>
2023-11-04 16:06:43 +0100

    changed navbar design

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import Card from "$lib/components/Card.svelte";
   import Navbar from "$lib/components/Navbar.svelte";
   import BlogTitle from "$lib/components/blog/BlogTitle.svelte";
   import navbarItemProp from "$lib/data/navbarItemProp";
@@ -10,7 +11,7 @@
 <div class="flex flex-col items-center">
   <Navbar navbarItems={navbarItemProp} />
   <BlogTitle title={data.meta.title} />
-  <div>
+  <div class="w-3/4">
     <svelte:component this={data.content} />
   </div>
 </div>
