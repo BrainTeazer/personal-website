ab655d4a0e75b2acad4e81a8e7a15634246097f3
AB <brainteazer123@gmail.com>
2023-11-26 14:23:17 +0100

    added breadcrumbs

--- svelte.config.js
+++ svelte.config.js
@@ -24,9 +24,9 @@ const config = {
       assets: "build",
       fallback: "index.html",
     }),
-    // paths: {
-    //   relative: false,
-    // },
+    paths: {
+      relative: false,
+    },
   },
 };
 
