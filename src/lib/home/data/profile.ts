273b9441a629207f8102c8129871a59bb2b80f6d
AB <brainteazer123@gmail.com>
2025-02-09 13:16:20 +0100

    Update GitHub actions and profile.ts

---
+++ src/lib/home/data/profile.ts
@@ -0,0 +1,27 @@
+import { fontConfig } from "$lib/common/data/theme/typography";
+import Github from "$lib/common/components/icons/Github.svelte";
+import Mail from "$lib/common/components/icons/Mail.svelte";
+import Linkedin from "$lib/common/components/icons/Linkedin.svelte";
+
+export default {
+  imgSrc: "./profile.jpeg",
+  name: "Ayam Banjade",
+  prelim: "Eclectic | Neophile | Programmer",
+  contactItems: [
+    {
+      name: "linkedin",
+      link: "https://www.linkedin.com/in/ayambanjade",
+      icon: Linkedin,
+    },
+    {
+      name: "github",
+      link: "https://github.com/BrainTeazer",
+      icon: Github,
+    },
+    {
+      name: "mail",
+      link: "mailto:***REMOVED***",
+      icon: Mail,
+    },
+  ],
+} as const;
